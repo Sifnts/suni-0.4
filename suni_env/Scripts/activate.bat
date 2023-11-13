d03ff2d42c5162945fa1cf155772f25bffdefc9b
Alfredo Sifuentes <alfrecgs@gmail.com>
2023-11-13 08:38:06 -0600

    .gitignore thing

--- suni_env/Scripts/activate.bat
+++ suni_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\CosasGithub\suni-0.4\suni_env
+set VIRTUAL_ENV=C:\Github Stuff\suni-0.4\suni_env
 
 if not defined PROMPT set PROMPT=$P$G
 
